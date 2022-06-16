99bc2be4b06f4387f2a0a84a8e7fdb8d2899478c
Pablo Cordova <darckman201@gmail.com>
2022-06-16 18:21:44 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_02',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
