85b5bdf58dc2beb5f6f6b925d85cb21ebeb443cf
Pablo Cordova <darckman201@gmail.com>
2022-06-16 19:07:03 -0500

    primer commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import ListPage from '../views/List.vue';
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
     redirect: '/home'
@@ -11,6 +11,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: ListPage
   }
 ]
 
---
+++ src/views/List.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>Lista</ion-title>
+            </ion-toolbar>
+        </ion-header>
+    </ion-page>
+
+
+</template>
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ListPage',
+});
+
+
+</script>
